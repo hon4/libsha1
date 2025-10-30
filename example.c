70ab61e08e7a3bd57941826368db3dd0598a4592
hon <greek.team.blog2@gmail.com>
2025-10-30 20:31:13 +0000

    Add example.c

---
+++ example.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sha1.h>
+
+int main() {
+    // Input string to hash
+    const unsigned char* input = "test";
+
+    // Calculate SHA-1 hash
+    unsigned char* hash = sha1(input);
+
+    // Print the SHA-1 hash in hexadecimal format
+    for (int i = 0; i < 20; i++) {
+        printf("%02X", hash[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
