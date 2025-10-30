e7d15716f12e3a58fb17b2ed972b58bb324f69f4
hon <greek.team.blog2@gmail.com>
2025-10-30 17:20:37 +0000

    Functional on x86 CPUs

--- sha1.h
+++ sha1.h
@@ -90,26 +90,83 @@ uint64_t calc_pad_size(uint64_t l) {
 	//return (l / 64) + 1;	//Method 1
 }
 
+uint32_t endian_le2be(uint32_t i) {
+	return ((i >> 24) & 0x000000FF) |
+			((i >> 8) & 0x0000FF00) |
+			((i << 8) & 0x00FF0000) |
+			((i << 24) & 0xFF000000);
+}
+
 uint8_t* sha1(uint8_t* str) {
 	uint64_t len = (uint64_t)strlen((char*)str);
 	uint64_t bit_len = len * 8;
 	uint64_t block_count = calc_pad_size(len + 9); //+9 = +1 for 0x80 and +8 for uint64
 	uint32_t M[block_count][16];
+	memset(M, 0x00, block_count * 16 * sizeof(uint32_t));
 
-	for (int block = 0; block < block_count; block++) {
-		for (int i = 0; i < 16; i++) {
-			uint32_t value = 0;
-			for (int b = 0; b < 4; b++) {
-				int byte_idx = block * 64 + i * 4 + b;
-				uint8_t byte = (byte_idx < len) ? str[byte_idx] : 0;
-				value |= (uint32_t)byte << (8 * (3 - b));  // big-endian assembly
-			}
-			M[block][i] = value;
-		}
+	/* Add the str into the M */
+	for (uint64_t i = 0; i < len; i++) {
+		((uint8_t*)M)[i] = str[i];
 	}
 
-	
+	((uint8_t*)M)[len] = 0x80; //Add the 0x80 = 0b10000000 at the end of the str
 
-	printf("%llu\n",len);
-	return 0;
+	((uint32_t*)M)[0] = endian_le2be(((uint32_t*)M)[0]);
+
+	/* Add the uint64_t bitlen at the last two uint32_t words of the last block */
+	// No endianess switch for the bit_len uint64(2 * uint32_t) because it's genned by the CPU and has the machines' arch already.
+	M[block_count-1][14] = (uint32_t)(bit_len >> 32);
+	M[block_count-1][15] = (uint32_t)(bit_len & 0xFFFFFFFF);
+
+
+	/* SHA1 Calculation using RFC3174 Method 1 */
+	uint32_t H0 = 0x67452301;
+	uint32_t H1 = 0xEFCDAB89;
+	uint32_t H2 = 0x98BADCFE;
+	uint32_t H3 = 0x10325476;
+	uint32_t H4 = 0xC3D2E1F0;
+
+	//uint32_t *W = (uint32_t*)(M[0]); //W points to the First M block
+	uint32_t W[80];
+	memset(W, 0x00, 80 * sizeof(uint32_t));
+	for (int i = 0; i < 16; i++) {
+		W[i] = M[0][i];
+	}
+
+	for (uint8_t t = 16; t <= 79; t++) { //t<=79 in x86 asm is checked with jle (1 instruction) but for other trashchip architectures like arm will need more than 1 instruction for check.
+		W[t] = sha1_snx(1, W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16]);
+	}
+
+	uint32_t A = H0;
+	uint32_t B = H1;
+	uint32_t C = H2;
+	uint32_t D = H3;
+	uint32_t E = H4;
+
+	for (uint8_t t = 0; t <= 79; t++) {
+		uint32_t TEMP = sha1_add(sha1_add(sha1_add(sha1_add(sha1_snx(5, A), sha1_funct(t, B, C, D)), E), W[t]), sha1_const(t));
+		E = D;
+		D = C;
+		C = sha1_snx(30, B);
+		B = A;
+		A = TEMP;
+	}
+
+	H0 = sha1_add(H0, A);
+	H1 = sha1_add(H1, B);
+	H2 = sha1_add(H2, C);
+	H3 = sha1_add(H3, D);
+	H4 = sha1_add(H4, E);
+	/* End RFC3174 Method 1 */
+
+	/* Convert H0, H1, H2, H3, H4 to a uint8_t* */
+	static uint8_t ret[20]; //20bytes is the SHA1 giest length
+	uint32_t* H = (uint32_t*)ret;
+	H[0] = endian_le2be(H0);
+	H[1] = endian_le2be(H1);
+	H[2] = endian_le2be(H2);
+	H[3] = endian_le2be(H3);
+	H[4] = endian_le2be(H4);
+
+	return ret;
 }
