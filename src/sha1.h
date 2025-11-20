97ba92f13b4b1be6fa3c24f09371e12076eea4a8
u0_a202 <u0_a202@localhost.(none)>
2025-11-21 00:21:36 +0200

    Update sha1.h include tags/infos

--- src/sha1.h
+++ src/sha1.h
@@ -1,3 +1,20 @@
+/*
++==================+
+| hon libSHA1      |
++==================+
+| Coded by: hon    |
+| Version: 0.0.1   |
+| Date: 2025-11-20 |
++------------------+
+*/
+/*
+ * This file (sha1.h) is part of hon's libSHA1
+ * Copyright (C) 2025 hon
+ *
+ * This program is free and open source software.
+ * License: GPL-2.0
+*/
+
 #ifndef LIBSHA1_SHA1_H
 #define LIBSHA1_SHA1_H
 
