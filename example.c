ffc04c97b1805b302012be54435e8a65381a974e
hon4 <greek.team.blog2@gmail.com>
2025-11-24 13:30:15 +0000

    Replace spaces with tabs

--- example.c
+++ example.c
@@ -2,17 +2,17 @@
 #include <sha1.h>
 
 int main() {
-    // Input string to hash
-    const unsigned char* input = (unsigned char*)"test";
+	// Input string to hash
+	const unsigned char* input = (unsigned char*)"test";
 
-    // Calculate SHA-1 hash
-    unsigned char* hash = sha1(input);
+	// Calculate SHA-1 hash
+	unsigned char* hash = sha1(input);
 
-    // Print the SHA-1 hash in hexadecimal format
-    for (int i = 0; i < 20; i++) {
-        printf("%02X", hash[i]);
-    }
-    printf("\n");
+	// Print the SHA-1 hash in hexadecimal format
+	for (int i = 0; i < 20; i++) {
+		printf("%02X", hash[i]);
+	}
+	printf("\n");
 
-    return 0;
+	return 0;
 }
