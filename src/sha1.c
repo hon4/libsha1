e63c853d97513782bbf49bde3301085e7560e5b3
hon4 <greek.team.blog2@gmail.com>
2025-11-28 20:46:22 +0000

    Update create SHA1 in parts (thanks mmxsrup)

--- src/sha1.c
+++ src/sha1.c
@@ -3,7 +3,7 @@
 ║ hon libSHA1      ║
 ╚══════════════════╝
 │ Coded by: hon    │
-│ Version: 0.0.3   │
+│ Version: 0.0.4   │
 │ Date: 2025-11-28 │
 └──────────────────┘
 */
@@ -14,6 +14,7 @@
  * This program is free and open source software.
  * License: GPL-2.0
 */
+// Thanks to mmxsrup (https://github.com/mmxsrup) for SHA1 Calculation in parts. Used ideas from https://github.com/mmxsrup/libsha1
 
 
 // Define Endianness (1=Little-Endian, 0=Big-Endian)
@@ -71,6 +72,9 @@ Notes: 1. In addition: if the Z variable is uint32_t can get values 0 <= z < 2^3
 | Addition            | Arithmetic | Z = (X + Y) mod 2^32             | = X + Y(when Z is a uint32_t) or Z = (X + Y) & 0xFFFFFFFF (when Z not uint32_t) |
 | Circular Left Shift | Bitwise    | S^n(X) = (X << n) OR (X >> 32-n) | = (X << n) | (X >> 32-n)                                                        |
 +---------------------+------------+----------------------------------+---------------------------------------------------------------------------------+
+
+Other Notes:
+1. Functions Statring with "sha1_" (lowercase) is functions from RFC3174, probably slightly modified to result in higher speeds (see above).
 */
 
 //The SHA1 Addition Function (auto pad to uint32 no need for mod 2^32 or and bitmask to 32bit
@@ -169,6 +173,7 @@ uint8_t* sha1(const uint8_t* str) {
 	uint64_t xblock;
 	for (xblock = 0; xblock < block_count; xblock++) {
 		memset(W, 0x00, 80 * sizeof(uint32_t)); //Clear the W
+		//Method 1 Step A
 		//Move the M[xblock] at the beggining of the W
 		int __libsha1_tmp_i2;
 		for (__libsha1_tmp_i2 = 0; __libsha1_tmp_i2 < 16; __libsha1_tmp_i2++) {
@@ -227,4 +232,115 @@ uint8_t* sha1(const uint8_t* str) {
 	#endif
 
 	return ret;
+}
+
+// SHA1 calculation in parts START
+void SHA1_Init(SHA1_CTX* context) {
+	context->H[0] = 0x67452301;	//H0
+	context->H[1] = 0xEFCDAB89;	//H1
+	context->H[2] = 0x98BADCFE;	//H2
+	context->H[3] = 0x10325476;	//H3
+	context->H[4] = 0xC3D2E1F0;	//H4
+	context->total_len = 0;
+}
+
+// Hash only one 512-bit/64-byte Block - Intalranl Use (Private function)
+void SHA1_Process_Block(SHA1_CTX* context) {
+	uint32_t W[80];
+	memset(W, 0x00, 80 * sizeof(uint32_t)); //Clear the W
+	//Method 1 Step A
+	//Move the M[xblock] at the beggining of the W
+	int __libsha1_tmp_i2;
+	uint32_t* buffer32 = (uint32_t*)context->buffer;
+	for (__libsha1_tmp_i2 = 0; __libsha1_tmp_i2 < 16; __libsha1_tmp_i2++) {
+		W[__libsha1_tmp_i2] = buffer32[__libsha1_tmp_i2];
+	}
+	
+	///// !!!! Probably Endianness-Switch here
+
+	//Method 1 Step B
+	uint8_t __libsha1_tmp_t1;
+	for (__libsha1_tmp_t1 = 16; __libsha1_tmp_t1 <= 79; __libsha1_tmp_t1++) { //t<=79 in x86 asm is checked with jle (1 instruction) but for other trashchip architectures like arm will need more than 1 instruction for check.
+		W[__libsha1_tmp_t1] = sha1_snx(1, W[__libsha1_tmp_t1-3] ^ W[__libsha1_tmp_t1-8] ^ W[__libsha1_tmp_t1-14] ^ W[__libsha1_tmp_t1-16]);
+	}
+
+	//Method 1 Step C
+	uint32_t A = context->H[0];
+	uint32_t B = context->H[1];
+	uint32_t C = context->H[2];
+	uint32_t D = context->H[3];
+	uint32_t E = context->H[4];
+
+	//Method 1 Step D
+	uint8_t __libsha1_tmp_t2;
+	for (__libsha1_tmp_t2 = 0; __libsha1_tmp_t2 <= 79; __libsha1_tmp_t2++) {
+		uint32_t TEMP = sha1_add(sha1_add(sha1_add(sha1_add(sha1_snx(5, A), sha1_funct(__libsha1_tmp_t2, B, C, D)), E), W[__libsha1_tmp_t2]), sha1_const(__libsha1_tmp_t2));
+		E = D;
+		D = C;
+		C = sha1_snx(30, B);
+		B = A;
+		A = TEMP;
+	}
+
+	//Method 1 Step E
+	context->H[0] = sha1_add(context->H[0], A);
+	context->H[1] = sha1_add(context->H[1], B);
+	context->H[2] = sha1_add(context->H[2], C);
+	context->H[3] = sha1_add(context->H[3], D);
+	context->H[4] = sha1_add(context->H[4], E);
+}
+
+void SHA1_Update(SHA1_CTX* context, const uint8_t* data) {
+	uint64_t len = (uint64_t)strlen(data);
+	uint8_t unhashed = context->total_len % 64; //64 = block size in bytes
+	
+	//len as remain_len
+	uint64_t processed_len = 0;
+	
+	if (unhashed + len < 64) {
+		return;
+	}
+	
+	while (unhashed + len > 63) {
+		memcpy(&context->buffer[unhashed], &data[processed_len], 64 - unhashed); //copy to fill buff
+		len -= 64 - unhashed;
+		processed_len += 64 - unhashed;
+		unhashed = 0;
+	}
+	
+	memcpy(&context->buffer[unhashed], &data[processed_len], len);
+	context->total_len += len;
+}
+
+void SHA1_Final(uint8_t digest[20], SHA1_CTX* context) {
+	//Prepare The Last Block
+	uint8_t unhashed = context->total_len % 64;
+	memset(&context->buffer[unhashed], 0x00, 64 - (unhashed * sizeof(uint8_t)));
+	
+	context->buffer[unhashed] = 0x80;			//The 0b10000000
+	uint64_t bit_len = context->total_len;		//The required bit len
+	
+	uint32_t* buffer32 = (uint32_t*)context->buffer;
+	buffer32[14] = (uint32_t)(bit_len >> 32);
+	buffer32[15] = (uint32_t)bit_len;
+	
+	//Hash the final block
+	SHA1_Process_Block(context);
+	
+	//Build the SHA1 digest
+	uint32_t* H = (uint32_t*)digest;
+	#if defined(__LIBSHA1_USE_ENDIANNESS__) && (__LIBSHA1_USE_ENDIANNESS__ == __LIBSHA1_LITTLE_ENDIAN__)
+	//Endianness change required only in Little-Endian
+	H[0] = endian_le2be(context->H[0]);
+	H[1] = endian_le2be(context->H[1]);
+	H[2] = endian_le2be(context->H[2]);
+	H[3] = endian_le2be(context->H[3]);
+	H[4] = endian_le2be(context->H[4]);
+	#else
+	H[0] = context->H[0];
+	H[1] = context->H[1];
+	H[2] = context->H[2];
+	H[3] = context->H[3];
+	H[4] = context->H[4];
+	#endif
 }
