dbda6958242f00587464d8757df8e7e6662534a5
u0_a202 <u0_a202@localhost.(none)>
2026-02-22 19:27:50 +0200

    Created build script for examples

---
+++ examples/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "[ INFO ] Starting example1.c compilation to e1."
+gcc example1.c -o e1 -lsha1
+echo "[  OK  ] Example1 compilation completed successfuly."
+
+echo "[ INFO ] Starting example2.c compilation to e2."
+gcc example2.c -o e2 -lsha1
+echo "[  OK  ] Example2 compilation completed successfuly."
