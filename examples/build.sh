545d11bff3984bbeeef93bf49c3fc0b6a40c3196
u0_a202 <u0_a202@localhost.(none)>
2026-02-22 19:33:20 +0200

    Added second error handler

--- examples/build.sh
+++ examples/build.sh
@@ -5,5 +5,5 @@ gcc example1.c -o e1 -lsha1 || { echo "[FAILED] Compilation of example1.c failed
 echo "[  OK  ] Example1 compilation completed successfuly."
 
 echo "[ INFO ] Starting example2.c compilation to e2."
-gcc example2.c -o e2 -lsha1
+gcc example2.c -o e2 -lsha1 || { echo "[FAILED] Compilation of example2.c failed."; exit 1; }
 echo "[  OK  ] Example2 compilation completed successfuly."
