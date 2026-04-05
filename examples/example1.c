84204747aca9306672003f5c315a8a059721bea6
root <root@localhost.my.domain>
2026-04-05 13:37:59 +0000

    Update example1.c to be compatible with std C89

--- examples/example1.c
+++ examples/example1.c
@@ -3,14 +3,15 @@
 #include <sha1.h>
 
 int main() {
-	// Input string to hash
+	/* Input string to hash */
 	const unsigned char* input = (unsigned char*)"test";
 
-	// Calculate SHA-1 hash
+	/* Calculate SHA-1 hash */
 	unsigned char* hash = sha1(input, strlen(input));
 
-	// Print the SHA-1 hash in hexadecimal format
-	for (int i = 0; i < 20; i++) {
+	/* Print the SHA-1 hash in hexadecimal format */
+	int i;
+	for (i = 0; i < 20; i++) {
 		printf("%02X", hash[i]);
 	}
 	printf("\n");
