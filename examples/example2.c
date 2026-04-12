d61a40b5d2ed0fb73bbcae21aa7d2dd72cd22826
u0_a202 <u0_a202@localhost.(none)>
2026-04-12 18:48:00 +0300

    Update example2.c: Make it C89 compatible

--- examples/example2.c
+++ examples/example2.c
@@ -3,19 +3,19 @@
 #include <sha1.h>
 
 int main() {
-    const unsigned char* input = (unsigned char*)"test";
-    SHA1_CTX context;
-    unsigned char hash[20];
+	const unsigned char* input = (unsigned char*)"test";
+	SHA1_CTX context;
+	unsigned char hash[20];
 
-    SHA1_Init(&context);
-    SHA1_Update(&context, input);  // Thanks to mmxsrup.
-    SHA1_Final(hash, &context);
+	SHA1_Init(&context);
+	SHA1_Update(&context, input);  /* Thanks to mmxsrup. */
+	SHA1_Final(hash, &context);
 
-    // Print the SHA-1 hash in hexadecimal format
-    for (int i = 0; i < 20; i++) {
-        printf("%02X", hash[i]);
-    }
-    printf("\n");
+	/* Print the SHA-1 hash in hexadecimal format */
+	for (int i = 0; i < 20; i++) {
+		printf("%02X", hash[i]);
+	}
+	printf("\n");
 
-    return 0;
+	return 0;
 }
