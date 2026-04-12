3bb2b42350f9bb098dd6f20f0a386273581ba339
u0_a202 <u0_a202@localhost.(none)>
2026-04-12 18:54:59 +0300

    Update Examples build.sh to use std C89

--- examples/build.sh
+++ examples/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "[ INFO ] Starting example1.c compilation to e1."
-gcc example1.c -o e1 -lsha1 || { echo "[FAILED] Compilation of example1.c failed."; exit 1; }
+gcc example1.c -o e1 -lsha1 -std=c89 || { echo "[FAILED] Compilation of example1.c failed."; exit 1; }
 echo "[  OK  ] Example1 compilation completed successfuly."
 
 echo "[ INFO ] Starting example2.c compilation to e2."
-gcc example2.c -o e2 -lsha1 || { echo "[FAILED] Compilation of example2.c failed."; exit 1; }
+gcc example2.c -o e2 -lsha1 -std=c89 || { echo "[FAILED] Compilation of example2.c failed."; exit 1; }
 echo "[  OK  ] Example2 compilation completed successfuly."
